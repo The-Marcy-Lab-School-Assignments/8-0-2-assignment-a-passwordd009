39c61018cbd0847f471b27648fc69dfa4a06ea7a
benspector-mls <ben@marcylabschool.org>
2024-07-02 14:30:40 -0400

    code graded: 11/13 tests passing

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,3 +1,6 @@
+/* FEEDBACK: Your API key is still exposed here! In your 
+GifSearch component, how can you update your backend to be able
+to handle search and remove this exposed key?*/
 const API_KEY = "5aAqDs1RGwolyoOsG912UmGPeUoVEOVl";
 
 export default API_KEY;
--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ const path = require('path');
 const key = process.env.API_KEY
 
 const serveGifs = async (req, res) => {
-        const [data] = await handleFetch(`https://api.giphy.com/v1/gifs/trending?api_key=${key}&limit=3&rating=g`)
-        res.send(data)
-  };
+  const [data] = await handleFetch(`https://api.giphy.com/v1/gifs/trending?api_key=${key}&limit=3&rating=g`)
+  res.send(data)
+};
 
 const pathToDistFolder = path.join(__dirname, '..', 'front-end', 'dist');
 const serveStatic = express.static(pathToDistFolder);
@@ -20,6 +20,6 @@ app.use(serveStatic);
 app.get('/api/gifs', serveGifs);
 
 const port = 8080;
-app.listen(port, () =>{
-    console.log(`Server is now running on http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Server is now running on http://localhost:${port}`)
 })
